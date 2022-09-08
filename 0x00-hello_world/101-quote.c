335fc81912c4bb28ebcc220d887fdcc022a1a9bd
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-08 13:31:58 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 1 (I guess standard error)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
