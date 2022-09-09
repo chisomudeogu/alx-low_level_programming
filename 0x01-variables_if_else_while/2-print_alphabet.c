6b457e75a03d7e6d5d4249d4c78e2233576fedc5
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-09 15:19:51 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
-
-/** 
+/**
  * main - Entry
  *
  * Return: Always 0 (Success)
  */
-
-int	main(void)
-
+int main(void)
 {
-
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
