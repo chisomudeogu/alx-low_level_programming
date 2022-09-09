b0b1ac9073e07a1c30272b0ce39f1d07d233ff85
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-09 13:25:56 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/** 
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int	main(void)
+
+{
+
+		int	n;
+
+		char	ch	=	'a';
+
+		while	(ch	<=	'z')
+		{
+			putchar("%c",	ch);
+			ch++;
+		}
+		return (0);
+}
