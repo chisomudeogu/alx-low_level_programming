af11835856b07d530552ca6b3c403db0a238a476
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-09 17:16:26 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main- Entry
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int digit1, digit2;
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
