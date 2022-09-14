e77ee6959c561d6de00f369b135bb95d56e3b117
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-14 16:38:36 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,6 @@
 
 /**
 * main - causes an infinite loop
-* @\\o/ - a character
 * Return: 0
 */
 
@@ -13,7 +12,6 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
 /*
 * while (i < 10)
 * {
