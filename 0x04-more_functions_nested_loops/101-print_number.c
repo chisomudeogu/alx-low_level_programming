5e51ddc8047e673710263680afbf0b90a629fabc
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-15 17:36:33 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,14 +12,14 @@ unsigned int a = n;
 
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 a = -a;
 }
 if (a > 9)
 {
-print_number(a / 10);
+_print_number(a / 10);
 }
-putchar(a % 10 + '0');
+_putchar(a % 10 + '0');
 }
 
 
