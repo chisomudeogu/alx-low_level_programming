55c889ff88b6b8d0857cdae5c7ea4d442c4c5bab
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-15 17:43:37 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,18 +8,22 @@
 
 void print_number(int n)
 {
-unsigned int a = n;
+unsigned int a;
 
 if (n < 0)
 {
+a = -n;
 _putchar('-');
-a = -a;
 }
-if (a > 9)
+else
 {
-_print_number(a / 10);
+a = n;
 }
-_putchar(a % 10 + '0');
+if (a / 10)
+{
+print_number(a / 10);
+}
+_putchar((a % 10) + '0');
 }
 
 
