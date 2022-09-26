50782a4487d2d7de2895ade914e5e2d327d03f16
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-26 16:52:52 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,13 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int d1 = 0, d2 = 0;
+	int i, j, d1 = 0, d2 = 0;
 
-	for (i = 0; i < size * size; i = 1 + (size + 1))
+	for (i = 0; i <= (size * size); i = 1 + size + 1)
 {
 		d1 = d1 + a[i];
 }
-	for (j = size - 1; j < size * size - 1; j = j + (size - 1))
+	for (j = size - 1; j <= (size * size) - size; j = j + size - 1)
 {
 		d2 = d2 + a[j];
 }
