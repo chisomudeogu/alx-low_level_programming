5b44b5691b4584663768e3478c4b2ea1f877132c
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-26 15:54:12 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: pointer that contains the address of the string
+* to be evaluated
+* @needle: pointer that contains the string to evaluate @haystack
+*
+* Return: address of the located string in @haystack
+* if no substring is found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int a;
+	int compare = 0;
+
+	if (*needle == 0)
+{
+		return (haystack);
+}
+	while (haystack[i] != 0)
+{
+		j = 0;
+		a = i;
+
+		while (needle[j] != 0)
+{
+			if (needle[j] == haystack[a])
+{
+				compare = 1;
+}
+			else
+{
+				compare = 0;
+				break;
+}
+		j++;
+		a++;
+}
+	if (compare == 1)
+{
+	return (haystack + 1);
+}
+	i++;
+}
+return (0);
+}
