a53d7d55918dc5ed72223f7e51931d45795c0ae3
chisomudeogu <chisomudeogu@yahoo.com>
2022-09-30 15:36:22 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+* @argc: amunt of argument passed through the program
+* @argv: pointer that contains the arrays of the arguments
+* Return: 0
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
